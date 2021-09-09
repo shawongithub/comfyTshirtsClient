92d9dc2ae5a4f9ea3ba5a89bc53174b64ae457d2
shawon <shawonislam121009@gmail.com>
2021-09-09 12:04:02 +0600

    added the login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './Login.css'
 
+import "firebase/auth";
+import firebaseConfig from '../../firebaseConfig';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+
+
 const Login = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    const googleLoginHandler = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+
+            }).catch((error) => {
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    }
     return (
         <div>
             <div className="login-container">
-                <input placeholder="Continue with google" type="text" />
+                <input value="Continue with google" type="submit" onClick={googleLoginHandler} />
             </div>
         </div>
     );
---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvCh68cuCPZGok4DAeGZlGYW3299-Q4uw",
+    authDomain: "auth-assignment-7e1fd.firebaseapp.com",
+    projectId: "auth-assignment-7e1fd",
+    storageBucket: "auth-assignment-7e1fd.appspot.com",
+    messagingSenderId: "307383923212",
+    appId: "1:307383923212:web:8ea5cbee1ece81709b3429",
+    measurementId: "G-LGT4NYVECF"
+};
+
+
+export default firebaseConfig
